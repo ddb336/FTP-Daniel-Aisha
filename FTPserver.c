48f2f621764adef2550b4aa6e5276d67d262bbe7
aisahodzic <ah4601@nyu.edu>
2021-03-01 14:49:56 +0400

    Updating select functionj

--- FTPserver.c
+++ FTPserver.c
@@ -5,14 +5,18 @@
 #include<sys/types.h>
  #include <unistd.h>
 #include<netinet/in.h>
+#include<stdlib.h>
+
+
+void serve_client(int client_sd);
 
 
 int main()
 {
-
+	
 	//1. socket
 	int server_fd = socket(AF_INET,SOCK_STREAM,0);
-	// printf("Server IN");
+	
 	if(server_fd<0)
 	{
 		perror("Socket: ");
@@ -25,6 +29,13 @@ int main()
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(9000);
 	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if(setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR,&(int){1},sizeof(int))<0)
+	{
+		perror("Setsockopt:");
+		return -1;
+	}
+
 	//2. bind
 	if(bind(server_fd,(struct sockaddr*) &server_address,sizeof(server_address))<0)
 	{
@@ -32,32 +43,31 @@ int main()
 		return -1;
 	}
 	//3. listen
-	if(listen(server_fd,2)<0)
+	if(listen(server_fd,5)<0)
 	{
 		perror("Listen: ");
 		return -1;
 	}
+
 	//4. accept
 	struct sockaddr_in client_address;				//we to pass this to accept method to get client info
 	int client_address_len = sizeof(client_address); // accept also needs client_address length
 	
 	char client_name[50];
 
-	int readyfd, nfds;
-	fd_set readfds;
+	int nfds;
+	fd_set readfds, readyfd;
 	//writefds, errorfds will be NULL for our task
 	// timeout is NULL, meaning we wait forever for response
 
 	// FD_ZERO(&fdset) initializes a descriptor set fdset to the null set.
 	FD_ZERO(&readfds); 
+	FD_SET(server_fd,&readfds);
 
 	//nfds=max num of fds, -> the descriptors up to and including nfds–1 are tested
-	// nfds = server_fd + 1;
+	int max_fd = server_fd; 
 	// FD_setsize also works
-	nfds = FD_SETSIZE; 
-
-	
-
+	// nfds = FD_SETSIZE; 
 	
 
 	while(1)
@@ -65,22 +75,48 @@ int main()
 		
 		// int select(int nfds, fd_set *restrict readfds, fd_set *restrict writefds,
   //        fd_set *restrict errorfds, struct timeval *restrict timeout);
-		FD_SET(server_fd, &readfds); 
-		readyfd = select(nfds, &readfds, NULL, NULL, NULL);
 
-		if (FD_ISSET(server_fd, &readfds)){
+		readyfd = readfds;
+        if(select(max_fd+1,&readyfd,NULL,NULL,NULL)<0)
+        {
+            perror("Select");
+            return -1;
+        }
 
-			// printf("CLIENT IN");
-			int client_fd = accept(server_fd, (struct sockaddr*)&client_address,(socklen_t *)&client_address_len);
+        for(int i=0; i<=max_fd;i++)		// In the end of the lab our loop terminating condition was i<max_fd that is why our server was not monitoring all sockets
+        {
+            if(FD_ISSET(i,&readyfd))
+            {
+
+                if(i==server_fd)
+                {
+                    int client_fd = accept(server_fd,NULL,NULL);
+                    FD_SET(client_fd,&readyfd);
+
+                    
+                    if(client_fd>max_fd) max_fd = client_fd;
+                }
+                else
+                {
+                    serve_client(i);
+                    FD_CLR(i,&readfds);
+                }
+            }
+        }
+
+}
+close(server_fd);
+	//5. send/receive 
+	//6. close the socket
+	
+}
+
+
+void serve_client(int client_fd){
 
 
 
-		char message[100];
-		char command[20];
-		char message_back[100];
-		
-
-		struct key_value
+	struct key_value
 			{
 			    char* key;
 			    char* value;
@@ -94,15 +130,10 @@ int main()
 		auth_users[1].key = "Daniel";
 		auth_users[1].value = "DBPass";
 
-		inet_ntop(AF_INET,&client_address.sin_addr,client_name,sizeof(client_name));
 
-		
-
-		if(client_fd<0)
-		{
-			perror("Accept: ");
-			return -1;
-		}
+	char message[100];
+	char command[20];
+	char message_back[100];
 
 		int pass_index = -1;
 
@@ -143,8 +174,7 @@ int main()
 						        	strcat(message_back,"Username OK, password required!");
 									
 							    	pass_index = i;
-							    	// printf("USEROK");
-							    	// printf("%s: %s \n",ptr, message_back);
+							    	
 							    	send(client_fd,message_back,strlen(message_back),0);
 
 				
@@ -215,14 +245,9 @@ int main()
 
 					}
 
-
+close(client_fd);		
 			
-		
-	}
-	}
+}	
 
-	close(server_fd);
-	//5. send/receive 
-	//6. close the socket
-	return 0;
-}
+		
+
